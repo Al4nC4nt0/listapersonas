d39dae1183005775fcf5fd0c55976a963cbad157
Alan Canto <mr.alancanto@gmail.com>
2024-09-12 22:47:58 -0300

    lista con boton elevado

---
+++ listapersonas/lib/model/persona.dart
@@ -0,0 +1,17 @@
+class Persona{
+  String nombre="";
+  int idade=-1;
+  String apellido="";
+  String cpf="";
+
+Persona(
+
+{
+  required this.nombre,
+  required this.idade,
+  required this.apellido,
+  required this.cpf,
+}
+);
+}
+
